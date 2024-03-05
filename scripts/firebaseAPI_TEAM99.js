0fcf4696a18234b9b14470bf63f23e3a334a509b
Alzen111 <alzenlandayan1616@gmail.com>
2024-03-05 09:04:41 -0800

    Add demo

--- scripts/firebaseAPI_TEAM99.js
+++ scripts/firebaseAPI_TEAM99.js
@@ -2,12 +2,13 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+    apiKey: "AIzaSyAM9HJQuFVsmLYq0ntagBmu0IwmxndUpWg",
+    authDomain: "comp1800-202410-demo-5b15c.firebaseapp.com",
+    projectId: "comp1800-202410-demo-5b15c",
+    storageBucket: "comp1800-202410-demo-5b15c.appspot.com",
+    messagingSenderId: "259056763502",
+    appId: "1:259056763502:web:f55724d70b843e5462dc7f",
+    measurementId: "G-G7ZB8VJC27"
 };
 
 //--------------------------------------------
